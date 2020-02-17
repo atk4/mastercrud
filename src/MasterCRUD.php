51bfa5b8f7d603a3e1e154fe0393cec5a8e67119
DarkSide <imants.horsts@inbox.lv>
2020-02-17 16:05:51 +0200

    proper tabs caption

--- src/MasterCRUD.php
+++ src/MasterCRUD.php
@@ -185,7 +185,7 @@ class MasterCRUD extends View
             }
             $m = $this->model->ref($ref);
 
-            $caption = $ref; // $this->getCaption($m);
+            $caption = $m->getModelCaption();
 
             $this->tabs->addTab($caption, function($p) use($subdef, $m, $ref) {
 
